51e647e851e94e8e84d27d2f7f3dd247691fb9d2
luke <luc.dewit@hotmail.nl>
2021-05-30 12:28:45 +0200

    Create new migration for door option id reference

---
+++ Rhinodoor-backend/Migrations/20210530102308_OrderDoorOptionMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rhinodoor_backend.Migrations
+{
+    public partial class OrderDoorOptionMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
